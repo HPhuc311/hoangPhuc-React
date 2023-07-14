d03239e74f34075362dc6ab1e448337f67afd348
HoangPhuc <hoangphuc311tn@gmail.com>
2023-07-14 21:45:42 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionComponent from './DemoComponent/FunctionComponent';
+import ClassComponent from './DemoComponent/ClassComponent';
 
 
-// compenent lớn nhất trong dự án
+// component lớn nhất trong dự án
+// 2 loại component 
+// + class component (stateful) < 2019 (lifecycle)
+// + function component (stateless) 100%
+// 1 component chỉ được phép return về 1 jsx duy nhất
+// attribute: viết theo nguyên tắc camelCase 
+
+// JSX: Javasript XML ==> file mở rộng viết html trong js 
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <span className='title' tabIndex>HELLO BC47</span>
+        <p></p>
+        <h1></h1>
+
+        <FunctionComponent></FunctionComponent>
+        <ClassComponent></ClassComponent>
     </div>
   );
 }
